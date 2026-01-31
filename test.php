2827245fddaf35d104f17fb672e8d8f235f3c9b9
Anushreecode <rautanushree35@gmail.com>
2026-01-31 18:08:32 +0545

    write the comment

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
+// variable is the component or part of code which store the data with defining specific name and datatype
 $bikes = array (1001, "Yamaha","KTM");
 var_dump($bikes);
 echo"<\br>";
